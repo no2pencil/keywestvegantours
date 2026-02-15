63fbc9b2e69f68dd733a3bb065900668e56f150c
EC2 Default User <ec2-user@ip-172-31-37-98.us-west-2.compute.internal>
2026-02-15 05:29:17 +0000

    Fixed pathing issue in index.js

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static('public'));
+app.use('/assets', express.static(path.join(__dirname, 'assets')));
 
 app.get('/', (req, res) => {
   res.send(`
